d6ce0b18d135aa39843fa0268598681fc8936a55
wenked <wenked.matheus@yahoo.com.br>
2020-07-08 14:08:12 -0300

    resources

--- src/components/ItemsInfo.tsx
+++ src/components/ItemsInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { itemChartPrices } from '../utils/types';
+import { Line } from 'react-chartjs-2';
 
 interface Props {
 	itemChartPrices: itemChartPrices[] | undefined;
@@ -7,10 +8,38 @@ interface Props {
 
 const ItemsInfo: React.FC<Props> = ({ itemChartPrices }) => {
 	return (
-		<div>
+		<div className='grid gap-4 grid-cols-2'>
 			{itemChartPrices?.map((item, i) => (
-				<div key={i}>
-					<p>{item.location}</p>
+				<div key={i} className='w-1/2 '>
+					<Line
+						data={{
+							labels: item.data.timestamps,
+							datasets: [
+								{
+									label: item.location,
+									showLines: true,
+									fill: false,
+									lineTension: 0.1,
+									backgroundColor: 'rgba(75,192,192,0.4)',
+									borderColor: 'rgba(75,192,192,1)',
+									borderCapStyle: 'butt',
+									borderDash: [],
+									borderDashOffset: 0.0,
+									borderJoinStyle: 'miter',
+									pointBorderColor: 'rgba(75,192,192,1)',
+									pointBackgroundColor: '#fff',
+									pointBorderWidth: 1,
+									pointHoverRadius: 5,
+									pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+									pointHoverBorderColor: 'rgba(220,220,220,1)',
+									pointHoverBorderWidth: 2,
+									pointRadius: 1,
+									pointHitRadius: 10,
+									data: item.data.prices_avg,
+								},
+							],
+						}}
+					/>
 				</div>
 			))}
 		</div>
--- src/components/ResourcesInfo.tsx
+++ src/components/ResourcesInfo.tsx
@@ -11,72 +11,100 @@ interface Props {
 	resourceData: profitArgs;
 }
 
+const organizeItems = (obj: Object, item: itemPriceData) => {
+	if (item !== undefined) {
+		return Object.assign(obj, {
+			[item.item_id]: item.item_id !== undefined && { ...item },
+		});
+	}
+
+	return Object.assign(obj, { nada: 'nada' });
+};
+
 const ResourcesInfo: React.FC<Props> = ({
 	rawResourceData,
 	refinedResourcesData,
 	resourceData,
 }) => {
+	const Teste: any = refinedResourcesData?.reduce(organizeItems, {});
+	refinedResourcesData !== undefined && console.log(Teste);
+	// Teste[getRecipe(rawResourceData.item_id).recipe].sell_price_min}
+	rawResourceData !== undefined &&
+		!rawResourceData.item_id.includes('T3') &&
+		console.log(
+			refinedResourcesData !== undefined &&
+				rawResourceData !== undefined &&
+				!rawResourceData.item_id.includes('T3') &&
+				Teste[getRecipe(rawResourceData?.item_id).recipe]
+		);
+
 	return (
 		<div className='block'>
-			{rawResourceData !== undefined && (
-				<div className='font-bold block text-xl py-4 rounded-sm'>
-					<div className='inline-flex'>
-						<img
-							src={`https://render.albiononline.com/v1/item/${rawResourceData?.item_id}?size=40`}
-							alt='item img'
-						/>
-						<p>Raw: {formatedItems[rawResourceData?.item_id]}</p>
-					</div>
-					<p>Raw Price: {rawResourceData?.sell_price_min}</p>
-					<div className='inline-flex'>
-						<img
-							src={`https://render.albiononline.com/v1/item/${
-								refinedResourcesData?.find(
-									refined =>
-										getRecipe(rawResourceData?.item_id).recipe ===
-										refined.item_id
-								)?.item_id
-							}?size=40`}
-							alt='item img'
-						/>
+			{refinedResourcesData !== undefined &&
+				rawResourceData !== undefined &&
+				!rawResourceData.item_id.includes('T3') && (
+					<div className='font-bold block text-xl py-4 rounded-sm'>
+						<div className='inline-flex'>
+							<img
+								src={`https://render.albiononline.com/v1/item/${rawResourceData?.item_id}?size=40`}
+								alt='item img'
+							/>
+							<p>Raw: {formatedItems[rawResourceData?.item_id]}</p>
+						</div>
+						<span className='text-red-500'>
+							{Teste[getRecipe(rawResourceData?.item_id).recipe].sell_price_min}
+						</span>
+						<p>Raw Price: {rawResourceData?.sell_price_min}</p>
+						<div className='inline-flex'>
+							<img
+								src={`https://render.albiononline.com/v1/item/${
+									refinedResourcesData?.find(
+										refined =>
+											getRecipe(rawResourceData?.item_id).recipe ===
+											refined.item_id
+									)?.item_id
+								}?size=40`}
+								alt='item img'
+							/>
+							<p>
+								Refined recipe price:{' '}
+								{
+									refinedResourcesData?.find(
+										refined =>
+											getRecipe(rawResourceData?.item_id).recipe ===
+											refined.item_id
+									)?.sell_price_min
+								}{' '}
+							</p>
+						</div>
 						<p>
-							Refined recipe price:{' '}
+							Refined price:{' '}
 							{
 								refinedResourcesData?.find(
 									refined =>
-										getRecipe(rawResourceData?.item_id).recipe ===
+										getRecipe(rawResourceData.item_id).refined ===
 										refined.item_id
 								)?.sell_price_min
-							}{' '}
+							}
 						</p>
+						<span className='text-green-700'>
+							Profit/Focus:{' '}
+							{getProfitPerFocus(
+								resourceData,
+								rawResourceData.sell_price_min,
+								refinedResourcesData?.find(
+									item =>
+										getRecipe(rawResourceData.item_id).recipe === item.item_id
+								)?.sell_price_min,
+								refinedResourcesData?.find(
+									item =>
+										getRecipe(rawResourceData.item_id).refined === item.item_id
+								)?.sell_price_min,
+								rawResourceData.item_id
+							)?.toFixed(2)}
+						</span>
 					</div>
-					<p>
-						Refined price:{' '}
-						{
-							refinedResourcesData?.find(
-								refined =>
-									getRecipe(rawResourceData.item_id).refined === refined.item_id
-							)?.sell_price_min
-						}
-					</p>
-					<span className='text-green-700'>
-						Profit/Focus:{' '}
-						{getProfitPerFocus(
-							resourceData,
-							rawResourceData.sell_price_min,
-							refinedResourcesData?.find(
-								item =>
-									getRecipe(rawResourceData.item_id).recipe === item.item_id
-							)?.sell_price_min,
-							refinedResourcesData?.find(
-								item =>
-									getRecipe(rawResourceData.item_id).refined === item.item_id
-							)?.sell_price_min,
-							rawResourceData.item_id
-						)?.toFixed(2)}
-					</span>
-				</div>
-			)}
+				)}
 		</div>
 	);
 };
